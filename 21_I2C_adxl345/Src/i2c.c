afa6cfd59b25f0c0cff83af53061128b3fc3623c
Gagik Navasardyan <g.navasardyan@outlook.com>
2026-08-24 17:16:43 -0700

    Fix project 21 I2C driver and expose its API in the header
    - Select AF4 on PB8/PB9 in AFRH. Without it the pins sit in alternate
      function mode pointing at AF0 (SYS), so SCL/SDA never reach the bus.
    - Add the missing empty body to the BTF wait in I2C1_burstWrite. The
      next statement was being swallowed as the loop body.
    - Correct 'size--' to 'n--' in I2C1_burstRead.
    - Silence the unused SR2 dummy reads.
    - Declare I2C1_init, I2C1_byteRead, I2C1_burstRead and I2C1_burstWrite
      in i2c.h, and move the stm32f4xx.h include above them.

--- 21_I2C_adxl345/Src/i2c.c
+++ 21_I2C_adxl345/Src/i2c.c
@@ -16,7 +16,7 @@
 #define CR1_STOP 				(1U<<9)
 #define SR1_RXNE 				(1U<<6)
 #define CR1_ACK 				(1U<<10)
-
+#define SR1_BTF 				(1U<<2)
 
 void I2C1_init(void)
 {
@@ -41,6 +41,10 @@ void I2C1_init(void)
 
 	GPIOB->PUPDR |= (1U<<18);
 	GPIOB->PUPDR &=~(1U<<19);
+
+	/* Set alternate function type in AFRH to AF04 (I2C1) for PB8 and PB9 */
+	GPIOB->AFR[1] |= (4U<<0);
+	GPIOB->AFR[1] |= (4U<<4);
 
 	/* enable clock access to I2C1 */
 	RCC->APB1ENR |= I2C1EN;
@@ -84,38 +88,161 @@ void I2C1_byteRead(char saddr, char maddr, char* data)
 
 	/* clear addr flag */
 	tmp = I2C1->SR2;
+	(void)tmp;
 
 	/* send memory address */
 	I2C1->DR = maddr;
 
 	/* wait until data register is empty */
-	while (!(I2C1->SR1 & SR1_TXE)) {}
+	while (!(I2C1->SR1 & (SR1_TXE))) {}
 
 	/* generate a re start condition */
 	I2C1->CR1 |= CR1_START;
 
 	/* wait until the start flag is set */
-	while (!(I2C1->SR1 & SR1_SB)){}
+	while (!(I2C1->SR1 & (SR1_SB))){}
 
 	/* Transmit slave address + Read */
 	I2C1->DR = saddr << 1 | 1;
 
 	/* wait until addr flag is set */
-	while (!(I2C1->SR1 & SR1_ADDR)){}
+	while (!(I2C1->SR1 & (SR1_ADDR))){}
 
 	/* disable acknowledge */
 	I2C1->CR1 &=~CR1_ACK;
 
 	/* clear addr flag */
 	tmp = I2C1->SR2;
+	(void)tmp;
 
 	/* Generate stop after data received */
 	I2C1->CR1 |= CR1_STOP;
 
 	/* wait until RXNE flag is set */
-	while (!(I2C1->SR1 & SR1_RXNE)){}
+	while (!(I2C1->SR1 & (SR1_RXNE))){}
 
 	/* read the data register and store it */
 	*data++ = I2C1->DR;
 
+}
+
+void I2C1_burstWrite(char saddr, char maddr, int n, char *data)
+{
+	volatile int tmp;
+
+	while (I2C1->SR2 & SR2_BUSY) {} // make sure our bus isn't busy.
+
+	/* Generate start */
+	I2C1->CR1 |= CR1_START;
+
+	/* wait for the start flag to be set */
+	while (!(I2C1->SR1 & (SR1_SB))){}
+
+	/* send slave address */
+	I2C1->DR = saddr << 1;
+
+	/* wait for addr flag to be set */
+	while(!(I2C1->SR1 & (SR1_ADDR))){}
+
+	/* clear the addr flag by reading SR2, we just read SR1. */
+	tmp = I2C1->SR2;
+	(void)tmp;
+
+	/* Wait until transmitter is empty */
+	while(!(I2C1->SR1 & (SR1_TXE))){} // done for best practice.
+
+	/* send memory address. write mode */
+	I2C1->DR = maddr;
+
+	/* enable the ACK bit */
+	I2C1->CR1 |= CR1_ACK;
+
+	for (int i = 0; i < n; i++)
+	{
+		/* wait until data register is empty */
+		while(!(I2C1->SR1 & (SR1_TXE))){}
+
+		I2C1->DR = *data++;
+	}
+
+	while (!(I2C1->SR1 & (SR1_BTF))){}
+
+	I2C1->CR1 &=~CR1_ACK;
+	I2C1->CR1 |= CR1_STOP;
+}
+
+void I2C1_burstRead(char saddr, char maddr, int n, char *data)
+{
+	volatile int tmp;
+
+	while (I2C1->SR2 & SR2_BUSY) {} // make sure our bus isn't busy.
+
+	/* Generate start */
+	I2C1->CR1 |= CR1_START;
+
+	/* wait for the start flag to be set */
+	while (!(I2C1->SR1 & (SR1_SB))){}
+
+	/* send slave address */
+	I2C1->DR = saddr << 1;
+
+	/* wait for addr flag to be set */
+	while(!(I2C1->SR1 & (SR1_ADDR))){}
+
+	/* clear the addr flag by reading SR2, we just read SR1. */
+	tmp = I2C1->SR2;
+	(void)tmp;
+
+	/* Wait until transmitter is empty */
+	while(!(I2C1->SR1 & (SR1_TXE))){} // done for best practice.
+
+	/* send memory address. write mode still */
+	I2C1->DR = maddr;
+
+	/* Wait until transmitter is empty */
+	while(!(I2C1->SR1 & (SR1_TXE))){} // done for best practice.
+
+	/* generate a re start condition */
+	I2C1->CR1 |= CR1_START;
+
+	/* wait until the start flag is set */
+	while (!(I2C1->SR1 & (SR1_SB))){}
+
+	I2C1->DR = saddr << 1 | 1; // send slave address in read mode
+
+	/* wait for addr flag to be set */
+	while (!(I2C1->SR1 & (SR1_ADDR))){}
+
+	/* clear addr flag */
+	tmp = I2C1->SR2;
+	(void)tmp;
+
+	/* enable the ACK bit */
+	I2C1->CR1 |= CR1_ACK;
+
+	while (n > 0)
+	{
+		if (n == 1)
+		{
+			/* pre-arm our stop condition after we get our data */
+			I2C1->CR1 &=~CR1_ACK; // disable ACK to send NACK on next send after we get our data.
+			I2C1->CR1 |= CR1_STOP;
+
+
+			/* wait until RXNE flag is set */
+			while (!(I2C1->SR1 & SR1_RXNE)){}
+
+			/* get our data */
+			*data++ = I2C1->DR;
+			break;
+		}
+		else
+		{
+			/* wait until RXNE flag is set */
+			while (!(I2C1->SR1 & SR1_RXNE)){}
+
+			*data++ = I2C1->DR; // get data as usual and increment the pointer.
+			n--;
+		}
+	}
 }
